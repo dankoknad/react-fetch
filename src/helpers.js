d5b83165e41a787015b4d0cb52182124766474e8
danko-m <dankomilutinovic@gmail.com>
2017-02-22 22:41:41 +0100

    Updates

--- src/helpers.js
+++
@@ -1,7 +0,0 @@
-export const capitalizeFirstLetter = (str) => {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-
-export const toTitleCase = (str) => {
-  return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-}
